cee9a3e8d193930bbd831a86d3ea2257907044c0
Sobolev Kirill <kirrok@mail.ru>
2016-10-11 15:44:06 +0300

    prepare project structure

---
+++ src/main/java/ru/mail/park/DbApiApplication.java
@@ -0,0 +1,12 @@
+package ru.mail.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbApiApplication.class, args);
+	}
+}
